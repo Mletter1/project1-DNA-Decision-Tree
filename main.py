36e2625086b92361d449c6795fbed552e4118a14
Matt Letter <mletter1@unm.edu>
2015-01-23 17:31:47 -0700

    update

--- main.py
+++ main.py
@@ -31,35 +31,39 @@ VERSION
     $Id$
 """
 __author__ = 'mletter1'
-import sys, os, traceback, optparse
+import sys
+import os
+import traceback
+import optparse
 import time
 import re
-#from pexpect import run, spawn
+# from pexpect import run, spawn
 
-def main ():
-
+def main():
     global options, args
     # TODO: Do something more interesting here...
     print 'Hello world!'
+
 
 if __name__ == '__main__':
     try:
         print 'Hello world!'
         start_time = time.time()
-        parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), usage=globals()['__doc__'], version='$Id$')
-        parser.add_option ('-v', '--verbose', action='store_true', default=False, help='verbose output')
+        parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), usage=globals()['__doc__'],
+                                       version='$Id$')
+        parser.add_option('-v', '--verbose', action='store_true', default=False, help='verbose output')
         (options, args) = parser.parse_args()
         if len(args) < 1:
-            parser.error ('missing argument')
+            parser.error('missing argument')
         if options.verbose: print time.asctime()
         main()
         if options.verbose: print time.asctime()
         if options.verbose: print 'TOTAL TIME IN MINUTES:',
         if options.verbose: print (time.time() - start_time) / 60.0
         sys.exit(0)
-    except KeyboardInterrupt, e: # Ctrl-C
+    except KeyboardInterrupt, e:  # Ctrl-C
         raise e
-    except SystemExit, e: # sys.exit()
+    except SystemExit, e:  # sys.exit()
         raise e
     except Exception, e:
         print 'ERROR, UNEXPECTED EXCEPTION'
